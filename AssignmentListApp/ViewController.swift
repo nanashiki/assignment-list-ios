115c7310503ef7d963aec5c688f03b5f079b7169
nanashiki <nanashiki.app@gmail.com>
2019-05-25 17:12:20 +0900

    Initial commit

---
+++ AssignmentListApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AssignmentListApp
+//
+//  Created by nanashiki on 2019/05/25.
+//  Copyright © 2019 nanashiki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
